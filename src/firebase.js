d75b1e03eb3804a01abd601f57f42a0148a380eb
Sizwe Mashele <seez.mash@gmail.com>
2022-03-13 19:33:23 +0200

    init

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCEaH9l8maJVEpqX4bYldqO1dy4xNoyPTk",
+  authDomain: "cloud-function-ninja.firebaseapp.com",
+  projectId: "cloud-function-ninja",
+  storageBucket: "cloud-function-ninja.appspot.com",
+  messagingSenderId: "49274524830",
+  appId: "1:49274524830:web:635a67a3bbca87e89d178f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
